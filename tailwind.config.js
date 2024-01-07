6f42c8bd320058ee6d55eedefa8161fb4653b649
Nahid1818 <155356380+Nahid1818@users.noreply.github.com>
2024-01-07 13:41:41 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+
+  darkMode: "media",
+
+  content: [],
+
+  theme: {
+    extend: {},
+  },
+
+  variants: {
+    extend: {}
+  },
+
+  plugins: [],
+}
+
